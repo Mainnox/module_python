335e804c4f30df1294ad84c80c843f68304493a5
Arnaud KREMER <akremer@e1r6p12.42.fr>
2021-11-22 17:35:29 +0100

    ex03 done but not exept for multiple arg or if not input handle

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -6,30 +6,27 @@ def isupper(c):
     return (0)
 
 def islower(c):
-    lowerw = "qwertyuiopadfghjklzxcvbnm"
+    lowerw = "qwertyuiopasdfghjklzxcvbnm"
     if (lowerw.find(c) != -1):
-        return (int(1))
-    return (int(0))
+        return (1)
+    return (0)
 
 def ispunc(c):
-    punc = ",.?'\";:-_"
+    punc = ",.!?'\";:-_"
     if (punc.find(c) != -1):
         return (1)
     return (0)
 
-
 def text_analyzer(string):
     'This function counts the number of upper characters, lower characters, punctuation and spaces in a given text.'
     space = 0
-    upper = int(0)
+    upper = 0
     lower = 0
     punc = 0
-
-    for i in enumerate(string):
-           if (string[i] == ' '):
-               space += 1
-            upper += isupper(string[i])
-            lower += islower(string[i])
-            punc += ispunc(string[i])
-    print("" + upper + lower + punc + space)
-    exit(0)
+    for i in range(0, len(string)):
+        if (string[i] == ' '):
+            space += 1
+        upper += isupper(string[i])
+        lower += islower(string[i])
+        punc += ispunc(string[i])
+    print("The text contains " + str(len(string)) + " characters:\n- " + str(upper) + " upper letters\n- " + str(lower) + " lower letters\n- " + str(punc) +" punctuation mark\n- " + str(space) + " spaces")
