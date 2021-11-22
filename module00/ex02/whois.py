415b77f9c71ac3dd0fa2e6d0be9cc606d9eac617
Arnaud KREMER <akremer@e1r6p12.42.fr>
2021-11-22 15:20:44 +0100

    Add ex02 clean and sound

--- module00/ex02/whois.py
+++ module00/ex02/whois.py
@@ -3,5 +3,14 @@ import sys
 if (__name__ == "__main__"):
     if (len(sys.argv) != 2):
         exit(2)
-    print(sys.argv[1].isdigit)
-    print("Tout finis !")
+    if (sys.argv[1].isdigit() == False):
+        print("ERROR")
+        exit(2)
+    number = int(sys.argv[1])
+    if (number == 0):
+        ret = "Zero"
+    elif (number % 2 == 0):
+        ret = "Even"
+    else:
+        ret = "Odd"
+    print("I'm " + ret + ".")
---
+++ module00/ex03/count.py
@@ -0,0 +1,4 @@
+def text_analyzer(string):
+    'This function counts the number of upper characters, lower characters, punctuation and spaces in a given text.'
+    print(string.count(['']))
+    exit(0)
