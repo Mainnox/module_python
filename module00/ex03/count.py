d26887b5dc762b0f7e9b60af11fd91f6418f7721
Arnaud KREMER <akremer@e1r6p12.42.fr>
2021-11-22 15:48:09 +0100

    Probleme synthaxe/logique du python

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,4 +1,35 @@
+
+def isupper(c):
+    upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    if (upper.find(c) != -1):
+        return (1)
+    return (0)
+
+def islower(c):
+    lowerw = "qwertyuiopadfghjklzxcvbnm"
+    if (lowerw.find(c) != -1):
+        return (int(1))
+    return (int(0))
+
+def ispunc(c):
+    punc = ",.?'\";:-_"
+    if (punc.find(c) != -1):
+        return (1)
+    return (0)
+
+
 def text_analyzer(string):
     'This function counts the number of upper characters, lower characters, punctuation and spaces in a given text.'
-    print(string.count(['']))
+    space = 0
+    upper = int(0)
+    lower = 0
+    punc = 0
+
+    for i in enumerate(string):
+           if (string[i] == ' '):
+               space += 1
+            upper += isupper(string[i])
+            lower += islower(string[i])
+            punc += ispunc(string[i])
+    print("" + upper + lower + punc + space)
     exit(0)
