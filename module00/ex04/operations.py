69b232a2ec4523b6ef4c92363119e0230f05826f
Arnaud KREMER <akremer@e1r2p1.42.fr>
2021-11-25 15:25:28 +0100

    ex04 safe and sound

--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -2,9 +2,26 @@ import sys
 
 def operations(nbr1, nbr2):
     'Usage: python operations.py <numbers1> <numbers2>\nExample:\n\tpython operations 12 3'
+    print('{0: <12}'.format('Sum:') + str(int(nbr1) + int(nbr2)))
+    print('{0: <12}'.format('Difference:') + str(int(nbr1) - int(nbr2)))
+    try:
+        print('{0: <12}'.format('Quotient:') + str(float(nbr1) / float(nbr2)))
+    except:
+        print('{0: <12}'.format('Quotient:') + "ERROR (Div by zero)")
+    try:
+        print('{0: <12}'.format('Remainder:') + str(int(nbr1) % int(nbr2)))
+    except:
+        print('{0: <12}'.format('Remainder:') + "ERROR (Mod vy zero)")
 
 if __name__ == "__main__":
     if (len(sys.argv) != 3):
-        print("InputError: too many arguments")
+        if (len(sys.argv) != 1):
+            print("InputError: too many arguments")
         print(operations.__doc__)
-    print(type(sys.argv[1]), type(sys.argv[2]))
+        exit(2)
+    try:
+        int(sys.argv[1]), int(sys.argv[2])
+    except:
+        print("InputError: Only numbers\n\n" + operations.__doc__)
+        exit(2)
+    operations(sys.argv[1], sys.argv[2])
